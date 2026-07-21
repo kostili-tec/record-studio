c282abdaf85b385e8a391e61da057edf35a4d9d9
kostili-tec <weh4teyou@gmail.com>
2026-07-21 14:04:30 +0300

    feat: add slider

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const swiper = new Swiper(".pricing-slider", {
+  loop: true,
+  spaceBetween: 24,
+
+  /* autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  }, */
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  navigation: {
+    prevEl: ".pricing-prev",
+    nextEl: ".pricing-next",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 3,
+    },
+  },
+});
